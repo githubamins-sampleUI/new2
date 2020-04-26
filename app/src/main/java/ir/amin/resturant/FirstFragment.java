6c0c99698e1b26ba015c02cdae9825d1eefc0916
aminsarlak <mohamadaminsarlak98@gmail.com>
2020-04-27 00:03:15 +0430

    i dont know

---
+++ app/src/main/java/ir/amin/resturant/FirstFragment.java
@@ -0,0 +1,27 @@
+package ir.amin.resturant;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class FirstFragment extends Fragment {
+
+    public FirstFragment() {
+    }
+
+    public FirstFragment initialize() {
+        return new FirstFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.sample,container,false);
+        return view;
+    }
+}
--- app/src/main/java/ir/amin/resturant/MainActivity.java
+++ app/src/main/java/ir/amin/resturant/MainActivity.java
@@ -1,14 +1,52 @@
 package ir.amin.resturant;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private ImageButton btn_basket, btn_home;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn_basket = findViewById(R.id.basket_btn);
+        btn_home = findViewById(R.id.home_btn);
+
+        btn_home.setOnClickListener(this);
+        btn_basket.setOnClickListener(this);
+        loadFragment(new FirstFragment());
+
+    }
+
+    private boolean loadFragment(Fragment fragment){
+        if (fragment != null){
+            getSupportFragmentManager().beginTransaction().replace(R.id.framelayout,fragment)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        Fragment f = null;
+        switch (view.getId()){
+            case R.id.basket_btn:
+                loadFragment(new FirstFragment());
+                Toast.makeText(getApplicationContext(), "clicke basket", Toast.LENGTH_SHORT).show();
+            break;
+
+            case R.id.home_btn:
+                loadFragment(new SecondFragment());
+                Toast.makeText(getApplicationContext(), "clicke home", Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
 }
